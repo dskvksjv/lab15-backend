67d3244934e59ecc34b5c41392ff973addcf9d07
VIKTORIIA REKONVALD <92644993+dskvksjv@users.noreply.github.com>
2024-12-09 15:18:33 +0100

    Delete src directory

--- src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { BookComponent } from './book.component';
-
-export const routes: Routes = [
-  { path: '', component: BookComponent },
-];
--- src/app/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    apiToken: 'FPLFaZpoM2JThJXOiw8AmoqrBqhBtt16V0sp3RbA',
-  };
-  
